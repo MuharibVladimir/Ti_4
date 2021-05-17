83e67d24814f63285fc5eccff544e8ca0bd78806
MuharibVladimir <67156221+MuharibVladimir@users.noreply.github.com>
2021-05-18 00:03:13 +0300

    upload project

---
+++ Lab4/LFSR.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+
+namespace Lab4
+{
+    public class LFSR
+    {
+        private ulong _lfsr;
+
+        private const int BufferLength = 2048;
+        private const int NumberOfBitsInByte = 8;
+        
+        public LFSR(ulong registerInitialValue)
+        {
+            _lfsr = registerInitialValue;
+        }
+
+        // x^24+x^4+x^3+x+1
+        private ulong GetRegisterBit()
+        {
+            var resultBit = _lfsr >> 31;
+            
+            var bit23 = (_lfsr >> 23) & 1;
+            var bit3 = (_lfsr >> 3) & 1;
+            var bit2 = (_lfsr >> 2) & 1;
+            var bit0 = (_lfsr >> 0) & 1;
+
+            var xor = bit0 ^ bit2 ^ bit3 ^ bit23;
+
+            _lfsr = (_lfsr << 1) | xor;
+
+            return resultBit;
+        }
+
+        public void Processing(string srcFile, string dstFile)
+        {
+            if (File.Exists(dstFile))
+                File.Delete(dstFile);
+            
+            var binaryReader = new BinaryReader(File.Open(srcFile, FileMode.Open));
+            var binaryWriter = new BinaryWriter(File.Open(dstFile, FileMode.Create));
+
+            try
+            {
+                var buffer = new byte[BufferLength];
+                int numberOfBytesRead;
+                do
+                {
+                    numberOfBytesRead = binaryReader.Read(buffer, 0, BufferLength);
+                    for (var i = 0; i < BufferLength; ++i)
+                    {
+                        for (var j = 0; j < NumberOfBitsInByte; ++j)
+                        {
+                            buffer[i] = (byte) (buffer[i] ^ (GetRegisterBit() << 7 - j));
+                        }
+                    }
+
+                    binaryWriter.Write(buffer, 0, numberOfBytesRead);
+
+                } while (numberOfBytesRead > 0);
+            }
+            finally
+            {
+                binaryReader.Close();
+                binaryWriter.Close();
+            }
+        }
+    }
+}
---
+++ Lab4/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+
+namespace Lab4
+
+{
+    public partial class MainWindow
+    {
+        private readonly Microsoft.Win32.OpenFileDialog _fileDialog = new Microsoft.Win32.OpenFileDialog();
+        private LFSR _lfsr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Preparation();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
+                return;
+            }
+
+            var destFile = TxtBoxPathToFile.Text + ".lfsr";
+            try
+            {
+                _lfsr.Processing(TxtBoxPathToFile.Text, destFile);
+            }
+            catch
+            {
+                Result.AppendText("Failed to encrypt file...\n");
+                return;
+            }
+
+            Result.AppendText("File successfully encrypted.\n");
+        }
+
+
+        private void Decrypt(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Preparation();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
+                return;
+            }
+
+            var index = TxtBoxPathToFile.Text.LastIndexOf('.');
+            var destFile = TxtBoxPathToFile.Text.Substring(0, index);
+            try
+            {
+                _lfsr.Processing(TxtBoxPathToFile.Text, destFile);
+            }
+            catch
+            {
+                Result.AppendText("Failed to decrypt file...\n");
+                return;
+            }
+
+            Result.AppendText("File successfully decrypted.\n");
+        }
+
+        private void Preparation()
+        {
+            if (!File.Exists(TxtBoxPathToFile.Text))
+            {
+                MessageBox.Show("File doesn't exists.", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+
+                throw new Exception();
+            }
+
+            _lfsr = new LFSR(ulong.Parse(Key.Text));
+        }
+
+        private void SelectFile(object sender, RoutedEventArgs e)
+        {
+            if (_fileDialog != null && (bool)_fileDialog.ShowDialog())
+            {
+                var fileName = _fileDialog.FileName;
+                TxtBoxPathToFile.Text = fileName;
+            }
+        }
+    }
+}
